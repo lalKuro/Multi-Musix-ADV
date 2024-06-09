3db98311e5520b354f0c9aa3554b38de13d6dd7a
Yannick <139569410+lalKuro@users.noreply.github.com>
2024-06-09 22:24:53 +0200

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -13,7 +13,7 @@ module.exports = {
       .setColor('#2b71ec')
       .setAuthor({
         name: 'Im here to Help!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
+        iconURL: 'https://cdn.discordapp.com/attachments/1140323365714927616/1243924242576310294/BRP-removebg-preview.png?ex=666704ba&is=6665b33a&hm=b4333f4043516e5c4c6f7b8142993f9351773a138cc318a69cc094b7d7ec75fa&', 
         url: 'https://discord.gg/dvWvS8kuWV'
     })
      
@@ -57,7 +57,7 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setImage(`https://cdn.discordapp.com/attachments/1140323365714927616/1243924242576310294/BRP-removebg-preview.png?ex=666704ba&is=6665b33a&hm=b4333f4043516e5c4c6f7b8142993f9351773a138cc318a69cc094b7d7ec75fa&`);
 
     const button1 = new ButtonBuilder()
       .setLabel('YouTube')
